34fc9e5bd920908c5eacaf9b0167be8747ee4351
Миллис <juliypuly@gmail.com>
2024-12-26 10:48:17 +0300

    Загрузка кодов

--- src/main/java/org/example/Cat.java
+++ src/main/java/org/example/Cat.java
@@ -1,4 +1,38 @@
 package org.example;
 
+import java.util.Random;
+
 public class Cat {
+    private Random random = new Random();
+
+    private void sleep() {
+        System.out.println("Sleep");
+    }
+
+    private void meow() {
+        System.out.println("Meow");
+    }
+
+    private void eat() {
+        System.out.println("Eat");
+    }
+
+    public void status() {
+        int option = random.nextInt(3);
+        switch (option) {
+            case 0:
+                sleep();
+                break;
+            case 1:
+                meow();
+                break;
+            case 2:
+                eat();
+                break;
+        }
+    }
 }
+
+
+
+
--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import org.example.Cat;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class CatTest {
+class CatTest {
+
+    @Test
+    void testStatus() {
+        Cat cat = new Cat();
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        for (int i = 0; i < 10; i++) {
+            cat.status();
+        }
+
+        String output = outContent.toString();
+        assertTrue(output.contains("Sleep") || output.contains("Meow") || output.contains("Eat"));
+    }
 }
+
